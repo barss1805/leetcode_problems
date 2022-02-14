f5eeeab953689ab6479822d29f136184e4d4fa8f
Vitali Klein <barss1805@mail.ru>
2022-02-14 10:57:24 +0300

    add task_description and solution

--- task_13/task_13.py
+++ task_13/task_13.py
@@ -1,70 +1,28 @@
-# 13. Roman to Integer --> EASY
-# https://leetcode.com/problems/roman-to-integer/
-# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-
-# Symbol       Value
-# I             1
-# V             5
-# X             10
-# L             50
-# C             100
-# D             500
-# M             1000
-
-# For example, 2 is written as II in Roman numeral, just two one's added together.
-# 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-# Roman numerals are usually written largest to smallest from left to right.
-# However, the numeral for four is not IIII. Instead, the number four is written
-# as IV. Because the one is before the five we subtract it making four.
-# The same principle applies to the number nine, which is written as IX.
-# There are six instances where subtraction is used:
-
-# I can be placed before V (5) and X (10) to make 4 and 9.
-# X can be placed before L (50) and C (100) to make 40 and 90.
-# C can be placed before D (500) and M (1000) to make 400 and 900
-
-# Given a roman numeral, convert it to an integer.
-
-# Example 1:
-# Input: s = "III"
-# Output: 3
-# Explanation: III = 3.
-
-# Example 2:
-# Input: s = "LVIII"
-# Output: 58
-# Explanation: L = 50, V= 5, III = 3.
-
-# Example 3:
-# Input: s = "MCMXCIV"
-# Output: 1994
-# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
-
-# Constraints:
-# 1 <= s.length <= 15
-# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
 class Solution:
     def roman_to_int(self, s: str) -> int:
-        latters_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-        }
-
+        letters_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        exc_dict = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+        number_out = 0
+        str_len = len(s)
+        if str_len < 2:
+            return letters_dict[s]
+        i = 1
+        while i < str_len:
+            check_letters = s[i-1] + s[i]
+            if check_letters in exc_dict:
+                number_out += exc_dict[check_letters]
+                i += 2
+            else:
+                number_out += letters_dict[s[i-1]]
+                i += 1
+        else:
+            if s[-2] + s[-1] not in exc_dict:
+                number_out += letters_dict[s[-1]]
+        return number_out
 
 
 if __name__ == '__main__':
-    test_str = "LVIII"
-
-
-    # test_str = "LVIII"
-    # Output: 58
+    test_str = "MCMXCIV"
     out = Solution()
     print(out.roman_to_int(test_str))
